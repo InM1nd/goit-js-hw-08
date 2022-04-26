dcb7832ba69e39bf171b67ce614000d448dc58d2
InM1nd <zabolotny1111@gmail.com>
2022-04-26 12:59:00 +0200

    Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const playerFrame = document.querySelector('#vimeo-player');
+
+const player = new Vimeo.Player(playerFrame);
+
+
+const onTimeUpdate = time => {
+    localStorage.setItem('videoplayer-current-time', time.seconds);
+    console.log(parseInt(localStorage.getItem('videoplayer-current-time')));
+};
+
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')) 
+
+.then(function(seconds) {
+    })
+.catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':    
+            break;
+        default:  
+            break;
+    }
+});
