ace754cf6d17af33f934678c53daf111d60dee22
InM1nd <zabolotny1111@gmail.com>
2022-04-29 21:34:21 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,47 @@
 import throttle from 'lodash.throttle';
 
+const KEY_STORAGE = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
-const formData = {};
-
-function onFormData(element) {
-  formData[element.target.name] = element.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}
-
-function onSubmitForm(element) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  element.preventDefault();
-  element.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-}
 
 function onSubmitForm(event) {
   event.preventDefault();
+
   const {
-    elements: { email, message }
+    elements: { email, message },
   } = event.currentTarget;
 
-  if (email.value === "" || message.value === "") {
-    return alert("Please fill all fields!");
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill all fields!');
   }
 
+  console.log(email.value, message.value);
+  localStorage.removeItem(KEY_STORAGE);
+  form.reset();
+}
+
+function onFormData(event) {
+  const parsedData = localStorage.getItem(KEY_STORAGE);
+  const data = parsedData ? JSON.parse(parsedData) :{};
+  const {elements: { email, message },
+  }  = event.currentTarget;
+  data.email = email.value;
+  data.message = message.value;
+
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(data));
 }
 
 (function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
+  const parsedData = localStorage.getItem(KEY_STORAGE);
+  if (parsedData) {
+    const parseMessage = JSON.parse(parsedData);
+    if (parseMessage) {
+      Object.keys(parseMessage).forEach(key => {
+        form.elements[key].value = parseMessage[key];
+      });
+    }
   }
 })();
 
